b0d80378bc58312573594964ef5bec90c5dec2ea
drew-worden <drew.p.worden@gmail.com>
2023-04-18 11:59:24 -0400

    Created a new Spring Boot application.

---
+++ src/main/java/learning/springbootrestfulwebservice/SpringbootRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package learning.springbootrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/springbootrestfulwebservice/SpringbootRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.springbootrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
